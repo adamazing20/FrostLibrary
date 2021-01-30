c422704130aeeb9d77e53f2d0e0f1d7bdd46c55a
adamazing20 <36868693+adamazing20@users.noreply.github.com>
2021-01-30 16:02:44 -0500

    trying to add lib to the dependency path

--- lib/src/providers/magic_armour_provider.dart
+++ lib/src/providers/magic_armour_provider.dart
@@ -2,8 +2,7 @@ part of 'item_provider.dart';
 
 class MagicArmourProvider extends ItemProvider<MagicArmour> {
   @override
-  String filePath = 'item_defs/magic_armour.yaml';
-
+  String filePath = 'lib/item_defs/magic_armour.yaml';
 
   @override
   Future load() async {
