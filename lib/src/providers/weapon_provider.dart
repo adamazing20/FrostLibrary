0483908f1a4c6d3576207a03550700011fc31a4b
adamazing20 <36868693+adamazing20@users.noreply.github.com>
2021-01-30 16:51:15 -0500

    lib back to weapons

--- lib/src/providers/weapon_provider.dart
+++ lib/src/providers/weapon_provider.dart
@@ -2,7 +2,7 @@ part of 'item_provider.dart';
 
 class WeaponProvider extends ItemProvider<Weapon> {
   @override
-  String filePath = '/item_defs/weapons.yaml';
+  String filePath = 'lib/item_defs/weapons.yaml';
 
   @override
   Future load() async {
