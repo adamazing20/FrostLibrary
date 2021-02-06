b429877fe93b81aee0c89f32a021313c7943f661
adamazing20 <36868693+adamazing20@users.noreply.github.com>
2021-02-06 14:29:26 -0500

    export needed types

--- lib/FrostLibrary.dart
+++ lib/FrostLibrary.dart
@@ -5,10 +5,14 @@ library FrostLibrary;
 
 // TODO: Export any libraries intended for clients of this package.
 export 'src/frost_library.dart';
+export 'src/models/character/apprentice/apprentice.dart';
+export 'src/models/character/character.dart';
+export 'src/models/character/charactertype.dart';
+export 'src/models/character/soldier/soldier.dart';
+export 'src/models/character/wizard/wizard.dart';
+export 'src/models/items/armour/amourtype.dart';
 export 'src/models/items/item/item.dart';
 export 'src/models/items/item/itemtype.dart';
-export 'src/models/character/charactertype.dart';
 export 'src/models/items/spell/school.dart';
 export 'src/models/items/weapons/weapontype.dart';
-export 'src/models/items/armour/amourtype.dart';
 export 'src/providers/definition_providers.dart';
